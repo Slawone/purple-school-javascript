8a1821f31e2733d4d4482b9c2f7f5614e86df934
Slawone <for1s8@ya.ru>
2022-11-24 21:12:41 +0300

    Упражнение - Проверка робота

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// Упражнение - Размещение депозита
+// Упражнение - Проверка робота
 
-const deposit = 12000;
-const rate = 0.07;
-const timeOfDeposit = 24;
-const houseCost = 13500;
+const message = prompt('Сколько будет 7 + или - 15?');
 
-const sum = (deposit * (1 + rate / 12) ** timeOfDeposit).toFixed(0);
-
-if (sum > 13500) {
-  console.log(`Вася накопил ${sum}$. Он может купить дом. У него останется ${sum - houseCost}$`);
-} else {
-  console.log(`Вася накопил ${sum}$. Купить не может.`);
+switch (true) {  
+  case message === 'Я не робот':
+  case Number(message) === -8:
+  case Number(message) === 22:
+    console.log('Успех');
+    break;
+  default:
+    console.log('Вы робот!');
 }
+  
