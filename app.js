57eb8d64ce82e36bafaaaa5eea48718aeaac26f6
Slawone <for1s8@ya.ru>
2022-11-24 15:43:54 +0300

    Упражнение - размещение депозита

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-// Задача - Расчет проекта
+// Упражнение - Размещение депозита
 
-const payRatedUSD = 80;
-const projectHours = 40;
-const availableHours = (11 - 2) * 5;
+const deposit = 12000;
+const rate = 0.07;
+const timeOfDeposit = 24;
+const houseCost = 13500;
 
-console.log('Смогу ли я работать ' + (availableHours > projectHours));
-console.log('Стоимость работ ' + projectHours * payRatedUSD + '$');
+const sum = (deposit * (1 + rate / 12) ** timeOfDeposit).toFixed(0);
+
+if (sum > 13500) {
+  console.log(`Вася накопил ${sum}$. Он может купить дом. У него останется ${sum - houseCost}$`);
+} else {
+  console.log(`Вася накопил ${sum}$. Купить не может.`);
+}
