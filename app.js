df5af76199fa6d2450817f9f8f73146328dfc648
Slawone <for1s8@ya.ru>
2022-11-24 13:49:12 +0300

    Раздел 4 - 6 Упражнение

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+// Задача - Расчет проекта
+
+const payRatedUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
+
+console.log('Смогу ли я работать ' + (availableHours > projectHours));
+console.log('Стоимость работ ' + projectHours * payRatedUSD + '$');
