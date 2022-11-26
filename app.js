1a2351dc1aed1a23b2805afa0ff09834c7f7fbae
Slawone <for1s8@ya.ru>
2022-11-26 16:02:33 +0300

    Упражнение - Обновление списка задач

--- app.js
+++ app.js
@@ -1,69 +1,57 @@
-// Упражнение - Кредит на MacBook
+const tasks = ['Задача 1'];
 
-function computeCredit(age, hasJob = false) {
-  switch(true) {
-    case age > 24 && hasJob:
-      return 500;
-    case age > 24:
-      return 100;
-    default:
-      return 0;
+function Add(task) {
+  tasks.push(task);
+}
+
+function Remove(task) {
+  const index = tasks.indexOf(task);
+  if (index === -1) {
+    return;
   }
+  tasks.splice(index, 1);
 }
 
-function canBuy(productPrice, age, money, hasJob = false) {
-  const creditMoney = computeCredit(age, hasJob);
-  return productPrice <= money + creditMoney;
+function Proritize(task) {
+  const index = tasks.indexOf(task);
+  if (index === -1) {
+    return;
+  }
+  const oldTask = tasks[index];
+  tasks.splice(index, 1);
+  tasks.unshift(oldTask);
 }
+Add('Задача 2');
+Add('Задача 3');
 
-console.log(canBuy(2000, 25, 1500, true));
+console.log(tasks);
 
-//--------------------------------------------------------------------------------------
-// Мое решение
+Remove('Задача 2');
 
-// const userAge = 30;
-// const isWorking = false;
-// const money = 1900;
-// function getCredit(age, work) {
-//   if (age > 24 && work === true) {
-//     return 500;
-//   } else if (age > 24) {
-//     return 100;
-//   } else {
-//     return 0;
-//   }
+console.log(tasks);
+
+Proritize('Задача 3');
+
+console.log(tasks);
+
+// const tasks = ['Задача 1', 'Задача 2', 'Задача 3', 'Задача 4', 'Задача 5'];
+
+// function addEl(arr, el) {
+//   arr.push(el);
+//   return arr;
 // }
 
-// function getMac(money, age, work) {
-//   if (money + getCredit(age, work) >= 2000) {
-//     return true;
-//   } else {
-//     return false;
-//   }
+// function delEl(arr, el) {
+//   arr.splice(arr.indexOf(el), 1);
+//   return arr;
 // }
 
-// console.log(getMac(money, userAge, isWorking));
-
-//--------------------------------------------------------------------------------------
-
-// Пример из урока
-
-// const KG_IN_USD = 7;
-// const KM_IN_USD = 5;
-
-// function calculateW(present) {
-//   return present * KG_IN_USD;
+// function reverseArr(arr) {
+//   return arr.reverse();
 // }
 
-// function calculateKm(distance) {
-//   return distance * KM_IN_USD;
-// }
+// // console.log(addEl(tasks, 'Задача 2'));
 
-// function getExchangePrice(present1, present2, distance) {
-//   const price1 = calculateW(present1);
-//   const price2 = calculateW(present2);
-//   const distancePrice = calculateKm(distance);
-//   return price1 + price2 + distancePrice;
-// }
+// // console.log(delEl(tasks, 'Задача 1'));
 
-// console.log(getExchangePrice(1, 2, 10)); 
+// console.log(reverseArr(tasks));
