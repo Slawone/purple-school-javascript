f7a6e64f413e243ba271ca08e87c56926950b8d5
Slawone <for1s8@ya.ru>
2022-11-30 22:08:11 +0300

    Функция разбора URl

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
-const tasks = ['Задача 1', 'Задача 2', 'Задача 3', 'Задача 4', 'Задача 5'];
+const url = 'https://purpleschool.ru/course/javascript';
 
-function Add(task) {
-  tasks.push(task);
+function getUrlComponents (url) {
+  const [protocol, _, host, ...path] = url.split('/');
+  console.log(`Протокол: ${protocol.split(':')[0]}`);
+  console.log(`Доменное имя: ${host}`);
+  console.log(`Путь внутри сайта: /${path.join('/')}`);
 }
 
-Add('Задача 6');
+getUrlComponents(url);
 
-console.log(tasks);
 
-function Remove(task) {
-  const index = tasks.indexOf(task);
-  if (index === -1) {
-    return;
-  }
-  return tasks.splice(index, 1);
-}
-
-Remove('Задача 4');
-console.log(tasks);
-
-function Prioritize(task) {
-  const res = Remove(task);
-  if (!res) {
-    return;
-  }
-  tasks.unshift(res[0]);
-}
-
-Prioritize('Задача 3');
-console.log(tasks);
+/*
+Дан произвольный url вида - https://purpleschool.ru/course/javascript
+Нужно сделать функцию, которая выводит в консоль:
+Протокол (https)
+Доменное имя (purpleschool.ru)
+Путь внутри сайта (/course/javascript)
+*/
