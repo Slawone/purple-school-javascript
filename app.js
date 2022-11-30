0e9c948a9ddcaad35af1097e451ddd4b703fd8ea
Slawone <for1s8@ya.ru>
2022-11-30 21:29:44 +0300

    Список задач DRY

--- app.js
+++ app.js
@@ -13,58 +13,19 @@ function Remove(task) {
   if (index === -1) {
     return;
   }
-  tasks.splice(index, 1);
+  return tasks.splice(index, 1);
 }
 
-Remove('Задача 10');
+Remove('Задача 4');
 console.log(tasks);
 
 function Prioritize(task) {
-  const index = tasks.indexOf(task);
-  if (index === -1) {
+  const res = Remove(task);
+  if (!res) {
     return;
   }
-  const oldEl = tasks[index];
-
-  tasks.splice(index, 1);
-
-  tasks.unshift(oldEl);
+  tasks.unshift(res[0]);
 }
 
-Prioritize('Задача 10');
+Prioritize('Задача 3');
 console.log(tasks);
-
-/*
-Сделать функции:
-Добавление задачи в конец
-Удаление задачи по названию
-Перенос задачи в начало списка по названию
-! Всегда меняем исходный массив
-*/
-
-
-
-
-
-
-// const tasks = ['Задача 1', 'Задача 2', 'Задача 3', 'Задача 4', 'Задача 5'];
-
-// function addEl(arr, el) {
-//   arr.push(el);
-//   return arr;
-// }
-
-// function delEl(arr, el) {
-//   arr.splice(arr.indexOf(el), 1);
-//   return arr;
-// }
-
-// function reverseArr(arr) {
-//   return arr.reverse();
-// }
-
-// // console.log(addEl(tasks, 'Задача 2'));
-
-// // console.log(delEl(tasks, 'Задача 1'));
-
-// console.log(reverseArr(tasks));
