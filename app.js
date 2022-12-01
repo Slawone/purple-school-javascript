ee077ee85f893b059283ed7349815f60ff946cb9
Slawone <for1s8@ya.ru>
2022-12-01 09:18:43 +0300

    Цикл в обратном порядке

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const url = 'https://purpleschool.ru/course/javascript';
+const arr = ['!', 'JS', 'люблю', 'Я'];
+const resultArray = [];
 
-function getUrlComponents (url) {
-  const [protocol, _, host, ...path] = url.split('/');
-  console.log(`Протокол: ${protocol.split(':')[0]}`);
-  console.log(`Доменное имя: ${host}`);
-  console.log(`Путь внутри сайта: /${path.join('/')}`);
+for (let i = arr.length - 1; i >= 0; i--) {
+  resultArray.push(arr[i]);
 }
 
-getUrlComponents(url);
+console.log(resultArray.join(' '));
 
 
-/*
-Дан произвольный url вида - https://purpleschool.ru/course/javascript
-Нужно сделать функцию, которая выводит в консоль:
-Протокол (https)
-Доменное имя (purpleschool.ru)
-Путь внутри сайта (/course/javascript)
-*/
+// let newArr = [];
+
+// for (let i = 0; i < arr.length; arr.length--) {
+//   newArr.push(arr[arr.length - 1]);
+// }
+// console.log(newArr.join(' '));
