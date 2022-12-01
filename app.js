8ac1137f851be1c96a8e5a612aa4d1e0d8c19047
Slawone <for1s8@ya.ru>
2022-12-01 14:32:11 +0300

    Упражнение - Стрелочные функции

--- app.js
+++ app.js
@@ -1,83 +1,11 @@
-const operations = [1000, -700, 300, -500, 10000];
-let startBalance = 100;
-
-function getBalance (arr, initialBalance) {
-  let balance = initialBalance;
-  for (const element of arr) {
-    balance += element;
-  }
-  return balance;
-}
-
-console.log(getBalance(operations, startBalance));
-
-function checkOperations(arr, initialBalance) {
-  let balance = initialBalance;
-  let isOk = true;
-  for (const element of arr) {
-    balance += element;
-    if (balance < 0) {
-      isOk = false;
-      break;
-    }
-  }
-  return isOk;
-}
-
-console.log(checkOperations(operations, startBalance));
-
-function avarageOperations (arr) {
-  let positiveCount = 0;
-  let negativeCount = 0;
-  let positiveSum = 0;
-  let negativeSum = 0;
-  for (const element of arr) {
-    if (element > 0) {
-      positiveCount++;
-      positiveSum += element;
-    }
-    if (element < 0) {
-      negativeCount++;
-      negativeSum += element;
-    }
-  }
-  return [positiveSum / positiveCount, negativeSum / negativeCount];
-} 
-
-console.log(avarageOperations(operations));
-
-// let negativeBalance = 0;
-
-// function getTotalBalance(arr) {
-//   for (element of arr) {
-//     startBalance += element;    
-//   }
-//   return startBalance;
-// }
-
-// console.log(getTotalBalance(operations));
-
-// function getNegativeBalance(arr) {
-//   getTotalBalance(arr);
-//   if (startBalance < 0) {
-//     console.log(false);
-//     return;
+// function power(pow) {
+//   return function(num) {
+//     return num ** pow;
 //   }
 // }
 
-// console.log(getNegativeBalance(operations));
+// console.log(power(4)(5));
 
-// let averageIncome = 0;
+const power = pow => num => num ** pow;
 
-// function getAverageCons (arr) {
-//   for (element of arr) {
-//     if (element >= 0) {
-//       averageIncome += element;
-//     }
-//   }
-//   averageIncome /= 3;
-// }
-
-// getAverageCons(operations);
-
-// console.log(averageIncome);
+console.log(power(4)(5));
